d892c814956b85a9479ce188b774ff1ad5c55bb6
1asingleone <1asingleone@users.noreply.github.com>
2026-06-16 23:52:32 +0800

    Remove temp file

--- check_cdn.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const c = fs.readFileSync('D:\\00草稿\\pdf2one\\pdf2one.html', 'utf8');
-const matches = c.match(/<script[^>]*src="[^"]*pdf-lib[^"]*"/g);
-if (matches) matches.forEach(s => console.log(s));
